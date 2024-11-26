0b26a64687ba0b90391ef588020826dcc7fc837a
arnaud <arnaud@Mac-mini-de-Arnaud-4.local>
2024-11-26 14:55:37 +0100

    fix: update configuration for Elestio deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  basePath: '',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
